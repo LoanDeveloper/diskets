0388985326000a96459c352f9dbe2dae3b6a217f
Robert <robertalphanor@gmail.com>
2025-02-11 20:29:47 +0100

    Correction de bug

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SelectCategory from "../components/SelectCategory";
 import SelectReason from "../components/SelectReason";
 import ExcuseList from "../components/ExcuseList";
@@ -11,10 +11,21 @@ const Home = () => {
 
     const [isGenerating, setIsGenerating] = useState(false);
 
+
     const handleGenerate =  () => {
         if (category && reason) {
             setIsGenerating(true);
         }
+    };
+
+    const handleCategoryChange = (newCategory) => {
+        setCategory(newCategory);
+        setIsGenerating(false);
+    };
+
+    const handleReasonChange = (newReason) => {
+        setReason(newReason);
+        setIsGenerating(false);
     };
 
     return (
@@ -22,8 +33,8 @@ const Home = () => {
             <h1>Diskets</h1>
             <p>Tu ne sais pas quoi inventer comme excuse ? Nous sommes là pour ça !</p>
             <div className="select-container">
-                <SelectCategory onSelect={setCategory} />
-                <SelectReason onSelect={setReason} />
+                <SelectCategory  onSelect={handleCategoryChange} />
+                <SelectReason onSelect={handleReasonChange} />
                 <button className="generate-button" onClick={()=> handleGenerate()}>
                     {isGenerating ? (
                         <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white">
