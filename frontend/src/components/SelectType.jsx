2d1de83c9566031344bedaede0d3af8115981593
Loan <loan.thomy@gmail.com>
2025-02-12 12:22:07 +0100

    connect select type with api

---
+++ frontend/src/components/SelectType.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+const SelectType = ({ onSelect }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState("");
+    const [types, setTypes] = useState([]);
+
+    useEffect(() => {
+        const fetchTypes = async () => {
+            try {
+                const response = await fetch("http://localhost:3000/types");
+                const data = await response.json();
+                setTypes(data.map(type => type.texte));
+            } catch (error) {
+                console.error("Erreur lors de la récupération des types", error);
+            }
+        };
+
+        fetchTypes();
+    }, []);
+
+    const handleSelect = (value) => {
+        setSelectedOption(value);
+        onSelect(value);
+        setIsOpen(false);
+    };
+
+    return (
+        <div className="select-box-container">
+            <div className="select-box" onClick={() => setIsOpen(!isOpen)}>
+                {selectedOption || "Quel type ?"}
+                <span className="arrow">▼</span>
+            </div>
+            {isOpen && (
+                <div className="options">
+                    {types.map((option) => (
+                        <div key={option} className="option" onClick={() => handleSelect(option)}>
+                            {option.charAt(0).toUpperCase() + option.slice(1)}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SelectType;
