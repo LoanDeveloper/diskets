1a608cca3b3c4de7d490558ea71b0e2ff4b75d4f
Loan <loan.thomy@gmail.com>
2025-02-14 11:16:56 +0100

    fix css

--- frontend/src/components/TextEditorWithSuggestions.jsx
+++ frontend/src/components/TextEditorWithSuggestions.jsx
@@ -37,7 +37,7 @@ const TextEditorWithSuggestions = ({ suggestions, onTextSelect, label }) => {
 
   return (
     <div >
-      {label}
+      <p className='label'>{label}</p>
       <input
         type="text"
         value={editorText}
