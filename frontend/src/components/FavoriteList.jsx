00326ea0d3bb5cc2ff24005c809e686595ef839b
Loan <loan.thomy@gmail.com>
2025-02-14 10:24:40 +0100

    fix copy FavoriteList

--- frontend/src/components/FavoriteList.jsx
+++ frontend/src/components/FavoriteList.jsx
@@ -3,47 +3,50 @@ import { ClipboardDocumentCheckIcon, CheckIcon } from "@heroicons/react/24/solid
 
 const FavoriteList = () => {
     const [favoris, setFavoris] = useState([]);
-    const [copied, setCopied] = useState(false);
+    // Utilise copiedId pour savoir quel item a été copié
+    const [copiedId, setCopiedId] = useState(null);
 
-    const handleCopy = async () => {
+    const handleCopy = async (text, id) => {
         try {
-            await navigator.clipboard.writeText(excuse);
-            setCopied(true);
-            setTimeout(() => setCopied(false), 2000);
+            await navigator.clipboard.writeText(text);
+            setCopiedId(id);
+            setTimeout(() => setCopiedId(null), 2000);
         } catch (err) {
             console.error("Erreur lors de la copie", err);
         }
     };
 
     useEffect(() => {
-        setCopied(false);
-
-        const fetchTypes = async () => {
+        const fetchExcuses = async () => {
             try {
                 const response = await fetch("http://localhost:3000/excuses");
                 const data = await response.json();
                 setFavoris(data);
-                console.log(favoris);
             } catch (error) {
                 console.error("Erreur lors de la récupération des excuses favorites", error);
             }
         };
 
-        fetchTypes();
+        fetchExcuses();
     }, []);
 
     return (
         <div className="excuse-list">
-                {favoris.map((favori) => (
-                    <div className="excuse-item-container">
-                        <div className="excuse-item">{favori.texte}</div>
-                        <button onClick={handleCopy} className="copy-button">
-                            <span>{copied ? <CheckIcon className="h-6 w-6 text-gray-500" /> : <ClipboardDocumentCheckIcon className="h-6 w-6 text-gray-500" />}</span>
-                        </button>
-                    </div>
-                ))}
+            {favoris.map((favori) => (
+                <div className="excuse-item-container" key={favori.id}>
+                    <div className="excuse-item">{favori.texte}</div>
+                    <button onClick={() => handleCopy(favori.texte, favori.id)} className="copy-button p-2 flex items-center justify-center">
+                        <span>
+                            {copiedId === favori.id ? (
+                                <CheckIcon className="h-6 w-6 text-green-500" />
+                            ) : (
+                                <ClipboardDocumentCheckIcon className="h-6 w-6 text-gray-500" />
+                            )}
+                        </span>
+                    </button>
+                </div>
+            ))}
         </div>
-        
     );
 };
 
